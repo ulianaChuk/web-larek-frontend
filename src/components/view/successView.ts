b17627b63cd2f2aa09567417a3820d03359d2390
Uliana Chukanova <uliana9904@yandex.ru>
2024-10-07 15:05:04 +0300

    chore: Refactor successView class to improve readability and maintainability

--- src/components/view/successView.ts
+++ src/components/view/successView.ts
@@ -2,19 +2,19 @@ import { IEvents } from "../base/events";
 
 export class SuccessModalView{
     success: HTMLElement;
-    description: HTMLElement;
-    button: HTMLButtonElement;
+    text: HTMLElement;
+    orderButton: HTMLButtonElement;
   
-    constructor(template: HTMLTemplateElement, protected events: IEvents) {
+    constructor(template: HTMLTemplateElement,  events: IEvents) {
       this.success = template.content.querySelector('.order-success').cloneNode(true) as HTMLElement;
-      this.description = this.success.querySelector('.order-success__description');
-      this.button = this.success.querySelector('.order-success__close');
+      this.text = this.success.querySelector('.order-success__description');
+      this.orderButton = this.success.querySelector('.order-success__close');
   
-      this.button.addEventListener('click', () => { events.emit('success:close') });
+      this.orderButton.addEventListener('click', () => { events.emit('successModal:close') });
     }
   
-    render(total: number) {
-      this.description.textContent = String(`Списано ${total} синапсов`);
+    render=(totalPrice: number)=> {
+      this.text.textContent = String(`Списано ${totalPrice} синапсов`);
       return this.success
     }
   }
